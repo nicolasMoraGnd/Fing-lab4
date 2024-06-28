94c22f0f5e3ae7b7b86f155701c0cba4cad2d5b5
nicolasMoraGnd <nicolasmoraern@gmail.com>
2024-06-27 21:31:03 -0300

    first hash tast completed

--- src/hashVisitaDia.cpp
+++ src/hashVisitaDia.cpp
@@ -1,7 +1,18 @@
 
 #include "../include/hashVisitaDia.h"
+typedef struct rep_nodoHash *nodoHash;
+
+struct rep_nodoHash {
+    TVisitaDia visita;
+    nodoHash sig;
+};
+
+
 
 struct rep_hashvisitadia {
+    int cantEstimada;
+    int cantidad;
+    nodoHash* nodos;
 };
 
 // interna funcion hash
@@ -10,7 +21,17 @@ int funcionHash(TFecha fecha, int cantEstimadas){
 }
 
 THashVisitaDia crearTHashVisitaDia(int cantEstimadas){
-    return NULL;
+    THashVisitaDia nuevoHash = new rep_hashvisitadia;
+    nuevoHash->nodos = new nodoHash[cantEstimadas];
+    nuevoHash->cantidad = 0;
+    nuevoHash->cantEstimada = cantEstimadas;
+
+    for (int i = 0; i < cantEstimadas; i++)
+    {
+        nuevoHash->nodos[i] = NULL;
+    }
+    
+    return nuevoHash;
 }
 
 void agregarVisitaDiaTHashVisitaDia(THashVisitaDia hash, TVisitaDia visitaDia){
@@ -27,5 +48,22 @@ bool perteneceVisitaDiaTHashVisitaDia(THashVisitaDia hash, TFecha fecha){
     return false;
 }
 
+void liberarNodoHash(nodoHash listaNodos){
+    if (listaNodos != NULL){
+        liberarNodoHash(listaNodos->sig);
+        liberarTVisitaDia(listaNodos->visita);
+        delete listaNodos;
+        listaNodos = NULL;
+    }    
+}
+
 void liberarTHashVisitaDia(THashVisitaDia &hash){
+    for (int i = 0; i < hash->cantEstimada; i++)
+    {
+        liberarNodoHash(hash->nodos[i]);
+
+    }
+    delete [] hash->nodos;
+    delete hash;
+    hash = NULL;
 }
